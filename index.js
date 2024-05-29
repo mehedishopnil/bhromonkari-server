8d1def70e13130a43cf28b2c67766417fd1dbea2
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-05-29 14:52:13 +0600

    new update

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connections::
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5cjch2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5cjch2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,51 +20,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-    // Get the database and collection on which to run the operation
-     
     const usersCollection = client.db("bhromonkariDB").collection("users");
 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-
-// Users related apis:
-app.get('/users', async(req, res, next) => {
-    const result = await usersCollection.find().toArray();
-    res.send(result);
-})
-
-
-app.post('/users', async(req, res) => {
-    const user = req.body;
-    const query = { email: user.email};
-    const existingUser = await usersCollection.findOne(query);
-    if (existingUser) {
-      res.status(400).send("User already exists");
-      return;
-    }
-    const result = await usersCollection.insertOne(user);
-    res.send(result)
-})
-
-
-
-
-
-
-
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.status(400).send("User already exists");
+        return;
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -71,9 +56,8 @@ app.post('/users', async(req, res) => {
 }
 run().catch(console.dir);
 
-// Define a route handler for the root route ("/")
 app.get("/", (req, res) => {
-  res.send("Hello from Bhromonkari Server!"); // Sending a simple response for demonstration
+  res.send("Hello from Bhromonkari Server!");
 });
 
 app.listen(port, () => {
