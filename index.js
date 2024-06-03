e3149eba6d81587ef2dc5f2af255a63a3471451c
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-06-03 08:52:58 +0600

    new update

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5cjch2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;`
 
 const client = new MongoClient(uri, {
@@ -54,8 +52,7 @@ async function run() {
       res.send(result);
     });
 
-
-    //Tour Place
+    // Tour Place
     app.get('/tourPlace', async (req, res) => {
       const result = await tourPlacesCollection.find().toArray();
       res.send(result);
