5552f8db82c0a190133926e9162d79ddeab73303
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-06-03 09:45:03 +0600

    Change port and update vercel.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
